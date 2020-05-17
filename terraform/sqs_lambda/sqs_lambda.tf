8a174b7d63c1a6e9344124abcc2f5ec862888c4f
Richard Julian <richard@rjulian.net>
2020-05-17 12:06:32 -0700

    Fix camel casing and source code dir

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -2,8 +2,8 @@ module "sqs_lambda" {
   source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v0.6.0"
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
-  function_name   = "DynamoDBPointInTimeRecoveryDisabled"
-  source_code_dir = "${path.module}/source"
+  function_name   = "DynamodbPointInTimeRecoveryDisabled"
+  source_code_dir = "${path.module}/../../source"
   handler         = "reflex_aws_dynamodb_point_in_time_recovery_disabled.lambda_handler"
   lambda_runtime  = "python3.7"
   environment_variable_map = {
@@ -28,10 +28,10 @@ EOF
 
 
 
-  queue_name    = "DynamoDBPointInTimeRecoveryDisabled"
+  queue_name    = "DynamodbPointInTimeRecoveryDisabled"
   delay_seconds = 0
 
-  target_id = "DynamoDBPointInTimeRecoveryDisabled"
+  target_id = "DynamodbPointInTimeRecoveryDisabled"
 
   sns_topic_arn  = var.sns_topic_arn
   sqs_kms_key_id = var.reflex_kms_key_id
