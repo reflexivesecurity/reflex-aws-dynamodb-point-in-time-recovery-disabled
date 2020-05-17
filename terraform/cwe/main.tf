51e0fd7ea6346f0f4ed0ba2881b0ffcc965d9191
Richard Julian <richard@rjulian.net>
2020-05-17 12:05:47 -0700

    Fix camelcasing for dynamodb

--- terraform/cwe/main.tf
+++ terraform/cwe/main.tf
@@ -1,6 +1,6 @@
 module "cwe" {
   source      = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.6.0"
-  name        = "DynamoDBPointInTimeRecoveryDisabled"
+  name        = "DynamodbPointInTimeRecoveryDisabled"
   description = "A Reflex Rule for ensuring enablement of Point in Time Recovery for DynamoDB tables."
 
   event_pattern = <<PATTERN
