36ab78c39faeec00049df3989cbf45e1dfb95d44
Max Anderson <maxwell.c.anderson@gmail.com>
2020-08-11 14:13:17 -0700

    Update to reflex-engine v2.0.0

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -1,11 +1,11 @@
 module "sqs_lambda" {
-  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v1.0.0"
+  source                    = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v2.0.0"
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
-  function_name   = "DynamodbPointInTimeRecoveryDisabled"
-  source_code_dir = "${path.module}/../../source"
-  handler         = "reflex_aws_dynamodb_point_in_time_recovery_disabled.lambda_handler"
-  lambda_runtime  = "python3.7"
+  function_name             = "DynamodbPointInTimeRecoveryDisabled"
+  package_location          = var.package_location
+  handler                   = "reflex_aws_dynamodb_point_in_time_recovery_disabled.lambda_handler"
+  lambda_runtime            = "python3.7"
   environment_variable_map = {
     SNS_TOPIC = var.sns_topic_arn,
     MODE      = var.mode
